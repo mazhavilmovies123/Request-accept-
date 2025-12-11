e329e1b0078c57922f6dfc5aa4ee7f64f3bda111
mazhavilmovies123 <muralirokz35@gmail.com>
2025-12-11 15:35:27 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,14 +3,14 @@ import os, time
 #from pyrogram.types import BotCommand
 
 class Config:
-    API_ID = int(getenv("API_ID", "7679954"))
-    API_HASH = getenv("API_HASH", "4af51af8f1a8b06ca2b076370ba93fba")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7344396275:AAFuEDN3U1v_1pboSapZFWEgy_-cXJVCglM")
-    UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "CinemaKalavaraTG")
-    UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-1001514228365"))
-    ADMIN = list(map(int, getenv("ADMIN", "7040444713 2144812475").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://hi:hi@auto.natzh.mongodb.net/?retryWrites=true&w=majority&appName=auto")
-    LOG_CHANNEL = -1002439306214
+    API_ID = int(getenv("API_ID", "21419016"))
+    API_HASH = getenv("API_HASH", "79198e1eb4cfd0f771a89d83b9144e7e")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8195734162:AAEDqrvuIgVqnPqEJ4yiDcwtMUW9K8lM3mQ")
+    UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "MazhavilMoviesUpdates")
+    UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-1002674451609"))
+    ADMIN = list(map(int, getenv("ADMIN", "1933114137").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://kentkouhali5l_db_user:gFvGsyASnQPu9rDZ@cluster0.m9xgtlr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    LOG_CHANNEL = -1003259271720
     
     #web response 
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
@@ -18,7 +18,7 @@ class Config:
     PORT = os.environ.get("PORT", "8080")
     
     DP_PIC = os.environ.get("DP_PIC", "https://envs.sh/_qj.mp4")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME","TGRequestAcceptBot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME","Mazhavilapproverbot")
  # Subsprice Gif & Auto Request Accept
     SURPRICE = os.environ.get("SURPRICE", "https://envs.sh/_qj.mp4").split()
 
